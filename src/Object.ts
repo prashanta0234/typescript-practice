b08c09ce3e702340986601951993cdf0cf95c557
prashanta0234 <prashanta0234@gmail.com>
2022-10-21 03:21:14 +0600

    object

---
+++ src/Object.ts
@@ -0,0 +1,20 @@
+// #################################### OBJECT ###################################
+
+// typescript think that this  is the stracture of person object
+
+const person = {
+  name: "pro",
+  age: 22,
+  isSleeping: false,
+};
+
+// allowed
+person.age = 20;
+
+// not allowed
+person.age = "nope";
+
+// not allowed
+person.computer = true;
+
+console.log(person);

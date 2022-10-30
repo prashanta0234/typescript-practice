e2d03fc13d85dae3f185703bdba822dfae2d6bdd
prashanta0234 <prashanta0234@gmail.com>
2022-10-30 21:33:42 +0600

    Generic class

--- output/GenericType.js
+++ output/GenericType.js
@@ -7,3 +7,21 @@ const passValue = data({
     id: 36,
 });
 console.log(passValue.id);
+// generic class
+class parentClass {
+    constructor() {
+        this.data = [];
+    }
+    set(a) {
+        return this.data.push(a);
+    }
+    remove(item) {
+        this.data.splice(this.data.indexOf(item));
+    }
+}
+const one = new parentClass();
+one.set(10);
+one.set("Pro");
+one.set(36);
+one.remove(36);
+console.log(one);
--- src/GenericType.ts
+++ src/GenericType.ts
@@ -8,3 +8,23 @@ const passValue = data({
 });
 
 console.log(passValue.id);
+
+// generic class
+
+class parentClass<T> {
+  data: T[] = [];
+
+  set(a: T) {
+    return this.data.push(a);
+  }
+  remove(item: T) {
+    this.data.splice(this.data.indexOf(item));
+  }
+}
+
+const one = new parentClass();
+one.set(10);
+one.set("Pro");
+one.set(36);
+one.remove(36);
+console.log(one);
